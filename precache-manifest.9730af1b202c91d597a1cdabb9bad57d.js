b7433018d2364be04d1cd21cb111f1648486c3ba
João Loureiro <jpmdl@hotmail.com>
2020-04-13 17:45:17 +0100

    Updates

---
+++ precache-manifest.9730af1b202c91d597a1cdabb9bad57d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "034cf223ace8b24e08a1631762ca338d",
+    "url": "/imove-gantt/index.html"
+  },
+  {
+    "revision": "00b8c9348f29235ada70",
+    "url": "/imove-gantt/static/js/2.0cc397e1.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/imove-gantt/static/js/2.0cc397e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4eb38b8bcab2e9e3ae9",
+    "url": "/imove-gantt/static/js/main.01be3c2d.chunk.js"
+  },
+  {
+    "revision": "71a8f62072b91609a598",
+    "url": "/imove-gantt/static/js/runtime-main.f01a4716.js"
+  }
+]);
